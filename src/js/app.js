09de8aa56a0a510f2762b1753bfb6f312121eb3c
MyCode07 <erem19992015@mail.ru>
2023-08-19 10:26:42 +0300

    first log

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import './parts/menu.js'
+import './parts/sliders.js'
+import './parts/gallery.js'
+import { stickyHeader } from './parts/header.js'
+import { toTop } from './static/to-top.js'
+import { animateImages } from './parts/image-aniamtions.js'
+
+stickyHeader() 
+toTop()
+animateImages()
