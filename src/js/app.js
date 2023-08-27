1ce9000a86279613f41447abcc46d7d5fe16f488
MyCode07 <erem19992015@mail.ru>
2023-08-27 14:02:18 +0300

    change text, logo

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,9 @@ import { stickyHeader } from './parts/header.js'
 import { toTop } from './static/to-top.js'
 import { animateImages } from './parts/image-aniamtions.js'
 import { notice } from './parts/notice.js'
+import { replaceDomElements } from './static/replace.js'
 
+replaceDomElements()
 stickyHeader() 
 toTop()
 animateImages()
